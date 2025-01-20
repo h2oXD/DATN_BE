0dd41f53bbad844b2fca06e6b5f85ac5605eb46b
HuuHao <fixbugandcry@gmail.com>
2025-01-20 09:36:30 +0700

    update migration

--- database/migrations/2025_01_15_083532_create_lecturers_table.php
+++ database/migrations/2025_01_15_083532_create_lecturers_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +15,7 @@ return new class extends Migration
         Schema::create('lecturers', function (Blueprint $table) {
             $table->id();
             
-            $table->foreignIdFor('user_id')->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
             $table->string('expertise')->nullable()->default(NULL);
             $table->decimal('rating', 2, 1)->default(0);
             $table->text('achievements')->nullable()->default(NULL);
