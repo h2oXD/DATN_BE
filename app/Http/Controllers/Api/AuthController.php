c3aaa0e2a6897293e461b79662eb24333d998562
Ngoc Hieu <nguyenngochieu2003@gmail.com>
2025-01-21 20:18:58 +0700

    21/1

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')],
-            'password' => ['required','min:8', 'confirmed' ]
+            'password' => ['required', 'min:8', 'confirmed']
         ]);
 
         if ($validator->fails()) {
@@ -46,9 +46,9 @@ class AuthController extends Controller
                 'role_id' => $role->id
             ]);
             $token = $user->createToken(__CLASS__)->plainTextToken;
-            
+
             DB::commit();
-            
+
             return response()->json([
                 'user' => $user,
                 'role' => $role,
