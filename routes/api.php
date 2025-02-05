1e121d93ed56a0bd79373489923d3f35acdc85b4
HuuHao <fixbugandcry@gmail.com>
2025-02-05 13:59:03 +0700

    fix user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DeepSeekController;
+use App\Http\Controllers\Api\V1\CourseController;
+use App\Http\Controllers\Api\V1\LecturerController;
 use App\Models\UserRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,9 +23,8 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:lecturer']], function () {
-    Route::get('/lecturer/dashboard', function (Request $request) {
-        return response()->json(['message' => 'Chào mừng Giảng viên']);
-    });
+    Route::get('/lecturer/dashboard', [LecturerController::class ,'dashboard']);
+    Route::post('/lecturer/course/create', [CourseController::class ,'lecturerCreateCourse']);
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:student']], function () {
     Route::get('/student/dashboard', function (Request $request) {
