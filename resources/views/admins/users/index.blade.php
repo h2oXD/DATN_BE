e8dfe25b86bbea5be3addda683e100483726a5fc
Ngoc Hieu <nguyenngochieu2003@gmail.com>
2025-02-08 20:26:52 +0700

    delete user

--- resources/views/admins/users/index.blade.php
+++ resources/views/admins/users/index.blade.php
@@ -19,7 +19,7 @@
                     </div>
                     <div class="col-lg-2 col-12 mb-2">
                         <select name="role" class="form-select ms-2 text-dark">
-                            <option value="">Chọn vai trò</option>
+                            <option value="">Tất cả vai trò</option>
                             <option value="1" {{ request('role') == '1' ? 'selected' : '' }}>
                                 Học viên</option>
                             <option value="2" {{ request('role') == '2' ? 'selected' : '' }}>
@@ -117,23 +117,21 @@
 
                                                     Sửa
                                                 </a>
-                                                <a class="dropdown-item" href="#">
+                                                <a href="javascript:void(0)" class="dropdown-item" onclick="confirmDelete({{ $user->id }})">
                                                     <svg class="w-10 me-2" xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                                                         class="size-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
                                                             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
-                                                    Xoá
+                                                    Xóa
                                                 </a>
+                                                <form id="delete-tag-{{ $user->id }}" action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:none;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                </form>
                                             </span>
-                                            {{-- <form action="{{ route('users.destroy', $user->id) }}" method="POST"
-                                                style="display:inline-block;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger"
-                                                    onclick="return confirm('Bạn có chắc chắn muốn xóa?')">Xóa</button>
-                                            </form> --}}
+
                                     </td>
                                 </tr>
                             @endforeach
@@ -145,3 +143,23 @@
         </div>
     </div>
 @endsection
+@section('script')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function confirmDelete(id) {
+            Swal.fire({
+                title: "Bạn có chắc chắn muốn xoá?",
+                text: "Hành động này không thể hoàn tác!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                confirmButtonText: "Xoá",
+                cancelButtonText: "Hủy"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById(`delete-tag-${id}`).submit();
+                }
+            });
+        }
+    </script>
