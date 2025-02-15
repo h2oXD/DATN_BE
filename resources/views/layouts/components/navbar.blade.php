b4d085f0a64cd5ad8ed2b9dffcb747ed5553fe5b
Tutt2004 <thaitux101234@gmail.com>
2025-02-15 13:12:55 +0700

    test

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -39,7 +39,7 @@
                 </p>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-dark" href="{{route('censor.corse.list')}}">
+                <a class="nav-link text-dark" href="{{ route('censor.courses.list') }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-10 me-2">
                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -193,7 +193,7 @@
                         </li>
                     </ul>
                 </div>
-                
+
             </li>
 
             <!-- Nav item -->
