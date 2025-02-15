b0c40bdf904ad32cab0d1d010375f1855b0067db
Van Thuyet <thiennguyenapk@gmail.com>
2025-02-15 12:57:03 +0700

    sua giao dien create danh muc

--- resources/views/admins/categories/create.blade.php
+++ resources/views/admins/categories/create.blade.php
@@ -33,33 +33,6 @@
                     <a href="{{ route('admin.categories.index') }}" class="btn btn-secondary">Quay lại</a>
                 </form>
             </div>
-            <div class="card-body">
-                <form action="{{ route('admin.categories.store') }}" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <label class="form-label">Tên danh mục</label>
-                        <input type="text" name="name" value="{{ old('name') }}"
-                            class="form-control @error('name') is-invalid @enderror">
-                        @error('name')
-                            <div class="text-danger mt-1">{{ $message }}</div>
-                        @enderror
-                    </div>
-
-                    <div class="mb-3">
-                        <label class="form-label">Danh mục cha</label>
-                        <select name="parent_id" class="form-control @error('parent_id') is-invalid @enderror">
-                            <option value="">Không có (danh mục gốc)</option>
-                            @foreach ($categories as $cat)
-                                <option value="{{ $cat->id }}">{{ $cat->name }}</option>
-                            @endforeach
-                        </select>
-
-                    </div>
-
-                    <button type="submit" class="btn btn-success">Thêm</button>
-                    <a href="{{ route('admin.categories.index') }}" class="btn btn-secondary">Quay lại</a>
-                </form>
-            </div>
         </div>
         </div>
 
