54bea5490f013e97617152fb1d2c16a9abd92380
Vu Duc Tai <vutai0122@gmail.com>
2025-02-20 05:43:15 +0700

    update wallet and vnpay

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,11 @@ use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\CourseController;
 use App\Http\Controllers\Api\V1\DocumentController;
 use App\Http\Controllers\Api\V1\LecturerController;
-use App\Http\Controllers\Api\V1\LecturerRegisterController;
 use App\Http\Controllers\Api\V1\LessonCodingController;
 use App\Http\Controllers\Api\V1\LessonController;
 use App\Http\Controllers\Api\V1\OverviewController;
 use App\Http\Controllers\Api\V1\QuizController;
 use App\Http\Controllers\Api\V1\SectionController;
-use App\Http\Controllers\Api\V1\StudyController;
 use App\Http\Controllers\Api\V1\TagController;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\VideoController;
@@ -35,27 +33,19 @@ use L5Swagger\Http\Controllers\SwaggerController;
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', [AuthController::class, 'getUser']);
-    Route::post('users', [UserController::class, 'update']);
-    Route::apiResource('user/wish-list', WishListController::class)->parameters(['wish-list' => 'wish-list_id']);
-    //wallet in user
-    Route::get('/user/wallet', [WalletController::class, 'show']);
-    Route::put('/user/wallet/{wallet_id}', [WalletController::class, 'update']);
-    Route::post('register/answers', [LecturerRegisterController::class, 'submitAnswers']);
-
     Route::apiResource('users', UserController::class)->only(['show', 'update']);
     Route::apiResource('user/wish-list', WishListController::class)->parameters(['wish-list' => 'wish-list_id']);
     //wallet in user
     Route::get('/user/wallets', [WalletController::class, 'show']);
-    Route::put('/user/wallets', [WalletController::class, 'update']);
+    Route::put('/user/wallets', [WalletController::class, 'update']); //test
+    Route::put('/user/wallets/deposit', [WalletController::class, 'depositPayment']);
+    Route::get('/user/wallets/result', [WalletController::class, 'resultPaymemt']);
 
     // Pay by VNPay
     Route::post('/user/courses/{course_id}/create-payment', [VNPayAPIController::class, 'createPayment']);
     Route::get('/user/courses/{course_id}/payment-callback', [VNPayAPIController::class, 'paymentCallback']);
     // Pay by wallet
     Route::post('/user/courses/{course_id}/wallet-payment', [WalletController::class, 'payment']);
-
-    //Study
-    Route::get('student/{user_id}/study/{courseId}', [StudyController::class, 'getCourseInfo']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
 });
@@ -71,17 +61,17 @@ Route::group(['middleware' => ['auth:sanctum', 'role:lecturer']], function () {
     Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/documents', DocumentController::class)->parameters(['documents' => 'document_id']);
     Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/codings', LessonCodingController::class)->parameters(['codings' => 'coding_id']);
 
-    Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/quizzes', QuizController::class)->parameters(['quizzes' => 'quiz_id']);
-    Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/quizzes/{quiz_id}/questions', QuizController::class)->parameters(['questions' => 'question_id']);
+    Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/quizzes',QuizController::class)->parameters(['quizzes' => 'quiz_id']);
+    Route::apiResource('/lecturer/courses/{course_id}/sections/{section_id}/lessons/{lesson_id}/quizzes/{quiz_id}/questions',QuizController::class)->parameters(['questions' => 'question_id']);
 
-    Route::post('lessons/order', [LessonController::class, 'updateOrder']);
+    Route::post('lessons/order',[LessonController::class, 'updateOrder']);
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:student']], function () {
     Route::get('/student/dashboard', function (Request $request) {
         return response()->json(['message' => 'Chào mừng Học viên']);
     });
 
-    Route::get('/student/home', [OverviewController::class, 'overview']);
+    Route::get('/student/home',[OverviewController::class, 'overview']);
 });
 Route::group(['middleware' => ['auth:sanctum', 'role:admin']], function () {
     Route::get('/admin/dashboard', function (Request $request) {
@@ -96,3 +86,4 @@ Route::get('/courses/{course_id}/public', [CourseController::class, 'publicCours
 Route::apiResource('/tags', TagController::class)->parameters(['tags' => 'tag_id']);
 
 Route::get('/api/documentation', [SwaggerController::class, 'api'])->name('l5-swagger.default.api');
+
