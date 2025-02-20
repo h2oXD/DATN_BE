7a763b6b066fed755b28a003d3e4ed822e54c765
Ngoc Hieu <nguyenngochieu2003@gmail.com>
2025-02-20 15:08:03 +0700

    fix api lesson & total_lessons trong section

--- database/migrations/2025_01_15_083640_create_sections_table.php
+++ database/migrations/2025_01_15_083640_create_sections_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->integer('order');
+            $table->integer('total_lessons')->default(0);
 
             $table->timestamps();
         });
